9901cc590422d4dfaef1e82d3285f7b362adeb12
Xl4yer <escolanojademark17@outlook.com>
2025-05-02 09:55:00 +0800

    5/2/2025 9:54 AM

---
+++ wwwroot/js/download.js
@@ -0,0 +1,8 @@
+﻿window.downloadFile = (filename, base64) => {
+    const link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," + base64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
